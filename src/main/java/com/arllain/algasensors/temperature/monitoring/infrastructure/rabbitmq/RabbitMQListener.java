5f20a3334b04d51c1b0f13b040e9d80f15afefb9
Arllain <arllain@gmail.com>
2025-11-02 13:42:26 -0300

    Múltiplas Queues para um consumidor

--- src/main/java/com/arllain/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/arllain/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
-import static com.arllain.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQCofing.QUEUE;
+import static com.arllain.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQCofing.QUEUE_ALERTING;
+import static com.arllain.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQCofing.QUEUE_PROCESS_TEMPERATURE;
 
 @Slf4j
 @Component
@@ -20,10 +21,19 @@ public class RabbitMQListener {
 
     private final TemperatureMonitoringService temperatureMonitoringService;
 
-    @RabbitListener(queues = QUEUE, concurrency = "2-3")
+    @RabbitListener(queues = QUEUE_PROCESS_TEMPERATURE, concurrency = "2-3")
     @SneakyThrows
-    public void handle(@Payload  TemperatureLogData temperatureLogData) {
+    public void handleProcessTemperature(@Payload  TemperatureLogData temperatureLogData) {
         temperatureMonitoringService.processTemperatureReading(temperatureLogData);
         Thread.sleep(Duration.ofSeconds(5));
     }
+
+    @RabbitListener(queues = QUEUE_ALERTING, concurrency = "2-3")
+    @SneakyThrows
+    public void handleAlerting(@Payload  TemperatureLogData temperatureLogData) {
+        temperatureMonitoringService.processTemperatureReading(temperatureLogData);
+        log.info("Alerting: SensorId {} Temp {}", temperatureLogData.getSensorId(), temperatureLogData.getValue());
+        Thread.sleep(Duration.ofSeconds(5));
+    }
+
 }
